9052f19e32e615e265797bf17f17724be9bf1f8b
sahanharshajith <sahanharshajith02@gmail.com>
2025-07-23 11:54:02 +0530

    Add MainBanner component with image slider and navigation
    - Implemented MainBanner component featuring a responsive image slider with dynamic text.
    - Added navigation arrows and dots indicator for slide navigation.
    - Integrated links to product page with styled buttons and icons.
    - Created a new SVG icon for the white arrow used in the button.
    - Developed NavBar component with user authentication and mobile menu functionality.
    - Included search bar and cart icon in NavBar for enhanced user experience.
    - Created Home page to render the MainBanner component.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import NavBar from './pages/NavBar';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const App = () => {
+
+  const isSellerPath = useLocation().pathname.includes('/seller');
+
   return (
     <div>
-      <NavBar />
+    {isSellerPath ? null : <NavBar />}
+      <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   );
 };
