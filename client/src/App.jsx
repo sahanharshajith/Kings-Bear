e05c4f3aef14ef5b734f4a3a83dacd985eeb1148
sahanharshajith <sahanharshajith02@gmail.com>
2025-07-27 12:50:48 +0530

    Add Login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,14 +6,18 @@ import { useLocation } from 'react-router-dom';
 import Footer from './components/Footer';
 import AboutUs from './pages/AboutUs';
 import ContactUs from './pages/ContactUs';
+import { useAppContext } from './context/AppContext.jsx';
+import Login from './components/Login.jsx';
 
 const App = () => {
 
   const isSellerPath = useLocation().pathname.includes('/seller');
+  const {showUserLogin} = useAppContext();
 
   return (
     <div>
     {isSellerPath ? null : <NavBar />}
+    {showUserLogin ? <Login /> : null}
       <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path="/" element={<Home />} />
